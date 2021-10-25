f1e7d48257c7046edc74c503425d0a1598bfbdd8
Rustam Kagarmanov ИВТ-21-2Б <92148756+ChaithTheRage@users.noreply.github.com>
2021-10-25 16:53:58 +0500

    Add files via upload

---
+++ Lab#2.py
@@ -0,0 +1,11 @@
+import random
+a = random.randint(1, 100)
+b = int(input("Введите число от 1 до 100\n"))
+c = 6
+for i in range(1, 6):
+    if b == a:
+        print("Поздравляю, вы нашли число")
+        break
+    else:
+        print("У вас осталось", c-i, "попыток")
+        b = int(input("Введите повторно число от 1 до 100\n"))

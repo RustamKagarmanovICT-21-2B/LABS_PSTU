6a242d7f45f072fa8ce473415d2519ef04ca2d18
ChaithTheRage <92148756+RustamKagarmanovICT-21-2B@users.noreply.github.com>
2021-10-25 17:55:35 +0500

    Update Lab#2.py

--- Lab#2.py
+++ Lab#2.py
@@ -1,11 +1,18 @@
 import random
 a = random.randint(1, 100)
-b = int(input("Введите число от 1 до 100\n"))
-c = 6
-for i in range(1, 6):
+c = int(input("Угадайте число от 1 до 100, для начала введите число попыток \n"))
+b = int(input('Введите предполагаемое число\n'))
+for i in range(1, c):
+    print(i, c)
     if b == a:
         print("Поздравляю, вы нашли число")
         break
-    else:
+    elif b != a:
         print("Количество оставшихся попыток:", c-i)
+        if b < a:
+            print("Введенное число меньше загаданного")
+        elif b > a:
+            print("Введенное число больше загаданного")
         b = int(input("Введите повторно число от 1 до 100\n"))
+        if i == c:
+            print('b')

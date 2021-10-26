44a8ca623521cc773eac2bd754436d8c044cb3c8
ChaithTheRage <92148756+RustamKagarmanovICT-21-2B@users.noreply.github.com>
2021-10-26 10:37:50 +0500

    Update LAB#1C++.cpp

--- LAB#1C++.cpp
+++ LAB#1C++.cpp
@@ -12,18 +12,12 @@ int A2 = (x % 100) / 10;
 int A3 = (x % 1000) / 100;
 if (x > 99 and x < 1000) {
 	if (A1 != A2 and A2 != A3 and A1 != A3) {
-		int a1 = A1 * 100 + A2 * 10 + A3;
-		int a2 = A1 * 100 + A3 * 10 + A2;
-		int a3 = A2 * 100 + A1 * 10 + A3;
-		int a4 = A2 * 100 + A3 * 10 + A1;
-		int a5 = A3 * 100 + A1 * 10 + A2;
-		int a6 = A3 * 100 + A2 * 10 + A1;
-		mass[0] = a1;
-		mass[1] = a2;
-		mass[2] = a3;
-		mass[3] = a4;
-		mass[4] = a5;
-		mass[5] = a6;
+		mass[0] = A1 * 100 + A2 * 10 + A3;
+		mass[1] = A1 * 100 + A3 * 10 + A2;
+		mass[2] = A2 * 100 + A1 * 10 + A3;
+		mass[3] = A2 * 100 + A3 * 10 + A1;
+		mass[4] = A3 * 100 + A1 * 10 + A2;
+		mass[5] = A3 * 100 + A2 * 10 + A1;
 		cout << "Перестановки " << endl;
 		cout << mass[0] << endl << mass[1] << endl << mass[2] << endl << mass[3] << endl << mass[4] << endl << mass[5] << endl;
 		int max1 = mass[0];

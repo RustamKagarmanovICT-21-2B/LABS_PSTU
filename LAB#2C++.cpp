6bdd97c4fb805cdf4552be9213fa273037d398c0
ChaithTheRage <92148756+RustamKagarmanovICT-21-2B@users.noreply.github.com>
2021-10-26 12:01:48 +0500

    Update LAB#2C++.cpp

--- LAB#2C++.cpp
+++ LAB#2C++.cpp
@@ -4,30 +4,30 @@
 using namespace std;
 int random(int a, int b)
 {
-	srand(time(NULL));
-	if (a > 0) return a + rand() % (b - a);
-	else return a + rand() % (abs(a) + b);
+srand(time(NULL));
+if (a > 0) return a + rand() % (b - a);
+else return a + rand() % (abs(a) + b);
 }
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	int temp;
-	int number = random(1, 100);
-	cout << "Угадайте число от 1 до 100" << endl;
-	for (int i = 1; i <= 7; i+=1) {
-		cout << 8 - i << " попыток осталось" << endl;
-		cin >> temp;
-		if (number != temp) {
-			if (temp > number)
-				cout << "Введенное число >" << endl;
-			else
-				cout << "Введенное число <" << endl;
+setlocale(LC_ALL, "Russian");
+int temp;
+int number = random(1, 100);
+cout << "Угадайте число от 1 до 100" << endl;
+for (int i = 1; i <= 7; i+=1) {
+	cout << 8 - i << " попыток осталось" << endl;
+	cin >> temp;
+	if (number != temp) {
+		if (temp > number)
+			cout << "Введенное число >" << endl;
+		else
+			cout << "Введенное число <" << endl;
 		}
-		else {
-			cout << "Поздравляю, вы отгадали число";
-			return 0;
-		}
+	else {
+		cout << "Поздравляю, вы отгадали число";
+		return 0;
 	}
-	cout << endl << "Вы не отгадали число, число - " << number;
-	return 0;
+}
+cout << endl << "Вы не отгадали число, число - " << number;
+return 0;
 }

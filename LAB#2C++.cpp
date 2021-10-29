91cdecc1b86172ab915fd8f7e5f4e9b6ee66076d
ChaithTheRage <92148756+RustamKagarmanovICT-21-2B@users.noreply.github.com>
2021-10-29 11:04:59 +0500

    Update LAB#2C++.cpp

--- LAB#2C++.cpp
+++ LAB#2C++.cpp
@@ -1,33 +1,29 @@
-﻿#include <iostream>
+#include <iostream>
 #include <clocale>
-#include <ctime>
+#include <time.h>
 using namespace std;
-int random(int a, int b)
+int main(void)
 {
-srand(time(NULL));
-if (a > 0) return a + rand() % (b - a);
-else return a + rand() % (abs(a) + b);
-}
-int main()
-{
-setlocale(LC_ALL, "Russian");
-int temp;
-int number = random(1, 100);
-cout << "Угадайте число от 1 до 100" << endl;
-for (int i = 1; i <= 7; i+=1) {
-	cout << 8 - i << " попыток осталось" << endl;
-	cin >> temp;
-	if (number != temp) {
-		if (temp > number)
-			cout << "Введенное число >" << endl;
-		else
-			cout << "Введенное число <" << endl;
+	int rnum;
+	setlocale(LC_ALL, "Russian");
+	srand(time(0));
+	rnum = rand() % 101;
+	cout << "Угадайте число от 1 до 100" << endl;
+	for (int i = 1; i <= 7; i += 1) {
+		int temp;
+		cout << 8 - i << " попыток осталось" << endl;
+		cin >> temp;
+		if (temp != rnum) {
+			if (rnum > temp)
+				cout << "Введенное число <" << endl;
+			else
+				cout << "Введенное число >" << endl;
 		}
-	else {
-		cout << "Поздравляю, вы отгадали число";
-		return 0;
+		else {
+			cout << "Поздравляю, вы отгадали число";
+			return 0;
+		}
 	}
-}
-cout << endl << "Вы не отгадали число, число - " << number;
-return 0;
+	cout << endl << "Вы не отгадали число, число - " << rnum;
+	return 0;
 }

8538172e37820041da1118fa6278ba22c4a6563f
ChaithTheRage <92148756+RustamKagarmanovICT-21-2B@users.noreply.github.com>
2021-11-03 15:23:07 +0500

    Add files via upload

---
+++ LAB4.2.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    //для использования русского языка
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите значение градусов\n";
+    //использование float для деления с запятой
+    float f;
+    cin >> f;
+    //формула для перевода градусов
+    f = (f - 32) * 5 / 9;
+    //вывод
+    cout << "Градусы по цельсьию - " << f;
+    return 0;
+}
